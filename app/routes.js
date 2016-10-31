b0600724b38e663eb35b9b9ac3e18ca9978c15bb
Manish <manish.bhatt@kuliza.com>
2016-11-01 00:48:26 +0530

    added events listing page and single event details page - dummy data for now

--- app/routes.js
+++ app/routes.js
@@ -4,12 +4,17 @@
 
 const express = require('express'),
   router = express.Router(),
-  mainController = require('./controllers/main.controller');
+  mainController = require('./controllers/main.controller'),
+  eventsController = require('./controllers/events.controller');
 
 // export the router
 module.exports = router;
 
 
 // define routes
-
+// main routes
 router.get('/', mainController.showHome);
+
+// event routes
+router.get('/events', eventsController.showEvents);
+router.get('/events/:slug', eventsController.showSingle);
