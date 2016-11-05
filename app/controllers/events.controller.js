ea5ae5ebc6fd83656192f6adeb3b6eec92dc86b9
Manish Bhatt <manishbhatt94@hotmail.com>
2016-11-05 15:12:51 +0530

    code cleanup

--- app/controllers/events.controller.js
+++ app/controllers/events.controller.js
@@ -1,27 +1,50 @@
 'use strict';
 
+const Event = require('../models/event');
+
 module.exports = {
-
-  // show all events
-  showEvents: (req, res) => {
-    // create dummy events
-    const events = [
-      { name: 'Basketball', slug: 'basketball', description: 'Throwing into a basket.' },
-      { name: 'Swimming', slug: 'swimming', description: 'Michael Phelps is the fast fish' },
-      { name: 'Weightlifting', slug: 'weightlifting', description: 'Lifting heavy things up.' },
-    ];
-
-    // return a view with data
-    res.render('pages/events', { events: events });
-  },
-
-  // show a single event
-  showSingle: (req, res) => {
-    // get a single event
-    const event = { name: 'Basketball', slug: 'basketball', description: 'Throwing into a basket.' };
-
-    // return a view with data
-    res.render('pages/single', { event: event });
-  }
-
+  showEvents: showEvents,
+  showSingle: showSingle,
+  seedEvents: seedEvents
 };
+
+
+// show all events
+function showEvents(req, res) {
+  // get all events
+  const events = [];
+
+  // return a view with data
+  res.render('pages/events', { events: events });
+}
+
+// show a single event
+function showSingle(req, res) {
+  // get a single event
+  const event = { name: 'Basketball', slug: 'basketball', description: 'Throwing into a basket.' };
+
+  // return a view with data
+  res.render('pages/single', { event: event });
+}
+
+// seed our database
+function seedEvents(req, res) {
+  // create some events
+  const events = [
+    { name: 'Basketball', description: 'Throwing into a basket.' },
+    { name: 'Swimming', description: 'Michael Phelps is the fast fish' },
+    { name: 'Weightlifting', description: 'Lifting heavy things up.' },
+    { name: 'Ping Pong', description: 'Super fast paddles' },
+  ];
+
+  // use the Event model to insert/save
+  Event.remove({}, () => {
+    for (var event of events) {
+      let newEvent = new Event(event);
+      newEvent.save();
+    }
+  });
+
+  // seeded!
+  res.send('Database seeded!');
+}
--- app/routes.js
+++ app/routes.js
@@ -2,9 +2,9 @@
 
 // create a new express router
 
-const express = require('express'),
-  router = express.Router(),
-  mainController = require('./controllers/main.controller'),
+const express      = require('express'),
+  router           = express.Router(),
+  mainController   = require('./controllers/main.controller'),
   eventsController = require('./controllers/events.controller');
 
 // export the router
@@ -17,4 +17,14 @@ router.get('/', mainController.showHome);
 
 // event routes
 router.get('/events', eventsController.showEvents);
+
+// seed events
+router.get('/events/seed', eventsController.seedEvents);
+
 router.get('/events/:slug', eventsController.showSingle);
+
+// create events
+
+// edit events
+
+// delete events
